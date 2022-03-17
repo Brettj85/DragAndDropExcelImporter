2c4d429f4aaef5af57def7c4351938815e5f034c
brettj85@gmail.com <brettj85@gmail.com>
2022-03-17 13:54:09 -0500

    added files

---
+++ DragAndDropUploader/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DragAndDropUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (var file in files)
+            {
+                string currentFile = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                string filePath = Path.Combine(Server.MapPath("~/UploadedFiles"), currentFile);
+                file.SaveAs(filePath);
+
+                FileInfo existingFile = new FileInfo(filePath);
+                using (ExcelPackage package = new ExcelPackage(existingFile))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                    int colCount = worksheet.Dimension.End.Column;  
+                    int rowCount = worksheet.Dimension.End.Row;
+                    for (int row = 1; row <= rowCount; row++)
+                    {
+                        for (int col = 1; col <= colCount; col++)
+                        {
+                            Debug.WriteLine(" Row:" + row + " column:" + col + " Value:" + worksheet.Cells[row, col].Value?.ToString().Trim());
+                        }
+                    }
+                }
+
+            }
+            return Json("file uploaded successfully");
+
+            
+        }
+    }
+}
